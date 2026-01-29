4f74db4a3b3a4dc838026bc97a2dd36c5f08fb37
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2026-01-29 14:49:30 -0500

    Delete src/features directory

--- src/features/feature_engineering.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def add_features(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.copy()
-    df["Returns"] = df["price"].pct_change()
-    df["Volatility"] = (df["High"] / df["Low"]) - 1
-    df.dropna(inplace=True)
-    return df[["Returns", "Volatility"]]
